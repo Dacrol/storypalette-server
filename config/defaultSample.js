e7b74adc20baafed171a5ac1e62fb3ee0ef472bd
erik <erik@unswornindustries.com>
2014-08-22 12:16:54 +0200

    Added sample config.

---
+++ config/defaultSample.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+// default = local development
+
+module.exports = {
+  api: {
+    baseUrl: 'http://api.storypalette.dev:8888/v1/',
+  },
+  server: {
+    environment: 'local',
+    port: 8888,
+    tokenSecret: 'SECRET_HERE', 
+    resources: path.resolve(__dirname, '../resources'), 
+    cache: path.resolve(__dirname, '../cache')
+  },
+  editor: {
+    folder: path.resolve(__dirname, '../../storypalette-editor/build'), 
+  },
+  player: {
+    folder: path.resolve(__dirname, '../../storypalette-player/build'), 
+  },
+  dmxplayer: {
+    server: 'http://localhost:8891' 
+  },
+  performer: {
+    folder: path.resolve(__dirname, '../../storypalette-performer-touch/build'), 
+  },
+  db: {
+    name: 'storypalette',
+    collections: ['users', 'palettes', 'organisations', 'resources']
+  }
+};
---
+++ config/production.js
@@ -0,0 +1,11 @@
+// Production server config
+
+module.exports = {
+  api: {
+    baseUrl: 'http://api.storypalette.net/v1/',
+  },
+  server: {
+    environment: 'production',
+    port: 8890
+  }
+};
